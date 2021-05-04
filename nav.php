ac8eba404824a0185f73dfd4c596a32772ac6cda
Hairak <77623242+Hairak@users.noreply.github.com>
2021-05-04 10:56:19 +0200

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,7 @@
+<a class="nav-link" href="index.php"><h1>Idiomas.</h1></a>
+
+<nav class="nav">
+  <a class="nav-link" href=""> Nouveautés</a>
+  <a class="nav-link" href=""> Streamer</a>
+  <a class="nav-link" href="login.php"> Se connecter</a>
+
